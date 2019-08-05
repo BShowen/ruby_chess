c37ee9a5d5cb7a4a8f1f7a3c2ba01f344d836def
Bradley Showen <BShowen@me.com>
2019-08-04 21:41:59 -0400

    legal moves populated from current position

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,24 +1,22 @@
 require_relative "basic_chess_piece"
+require_relative "custom_enumerable"
 
 class Knight < BasicChessPiece
+    include CustomEnumerable
 
-    def build_adj_list   
-        completed_list = []
-        for x in 0..7
-            for y in 0..7
-                temp_list = []
-                temp_list << [x - 2,y - 1] unless (x - 2 < 0 || y - 1 < 0)
-                temp_list << [x - 2,y + 1] unless (x - 2 < 0 || y + 1 > 7)                 
-                temp_list << [x + 2,y - 1] unless (x + 2 > 7 || y - 1 < 0)
-                temp_list << [x + 2,y + 1] unless (x + 2 > 7 || y + 1 > 7)                 
-                temp_list << [x - 1,y - 2] unless (y - 2 < 0 || x - 1 < 0)
-                temp_list << [x + 1,y - 2] unless (y - 2 < 0 || x + 1 > 7)
-                temp_list << [x - 1,y + 2] unless (y + 2 > 7 || x - 1 < 0)                 
-                temp_list << [x + 1,y + 2] unless (y + 2 > 7 || x + 1 > 7) 
-                completed_list << temp_list          
-            end
-        end
-        completed_list
+    def get_legal_moves(current_coordinates)
+        adj_list = []
+        x = current_coordinates[0]
+        y = current_coordinates[1]
+        adj_list << [x - 2,y - 1] if (x - 2 >= 0 && y - 1 >= 0) &&  square([x - 2, y - 1]).empty? 
+        adj_list << [x - 2,y + 1] if (x - 2 >= 0 && y + 1 < 8)  && square([x - 2, y + 1]).empty?               
+        adj_list << [x + 2,y - 1] if (x + 2 < 8 && y - 1 >= 0) && square([x + 2, y - 1]).empty?
+        adj_list << [x + 2,y + 1] if (x + 2 < 8 && y + 1 < 8) &&  square([x + 2, y + 1]).empty? 
+        adj_list << [x - 1,y - 2] if (y - 2 >= 0 && x - 1 >= 0) && square([x - 1, y - 2]).empty?
+        adj_list << [x + 1,y - 2] if (y - 2 >= 0 && x + 1 < 8) && square([x + 1, y - 2]).empty?
+        adj_list << [x - 1,y + 2] if (y + 2 < 8 && x - 1 >= 0) && square([x - 1, y + 2]).empty?                 
+        adj_list << [x + 1,y + 2] if (y + 2 < 8 && x + 1 < 8) && square([x + 1, y + 2]).empty?      
+        adj_list
     end
 
 end
