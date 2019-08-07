174762c78ce00030d60e5e75b62974eb91ccb007
Bradley Showen <BShowen@me.com>
2019-08-07 18:28:15 -0400

    remove uneeded functions

--- lib/basic_chess_piece.rb
+++ lib/basic_chess_piece.rb
@@ -2,38 +2,20 @@ require_relative "custom_enumerable"
 
 class BasicChessPiece 
     include CustomEnumerable
-    attr_accessor :character, :color
+    attr_accessor :character, :color, :current_position
 
     def initialize(character, color, current_position)
-        @color = color
         @character = character
+        @color = color
         @current_position = current_position 
-        # @legal_moves_from_current_pos = get_legal_moves(@current_position)
     end
-
-    def get_index_from_coordinates(coordinates)
-        index = coordinates.join.to_i
-        if index <= 7
-            index
-        elsif index <= 17
-            index - 2
-        elsif index <= 27
-            index - 4
-        elsif index <= 37
-            index - 6
-        elsif index <= 47
-            index - 8
-        elsif index <= 57
-            index -10
-        elsif index <= 67
-            index - 12
-        elsif index <= 77
-            index - 14
-        end
-    end 
-
+    
     def constraints_are_met?(x,y)
         square([x, y]).empty? == true || square([x, y]).piece.color != self.color
+    end
+
+    def legal_move?(desired_coordinates)
+        self.get_legal_moves(self.current_position).include?(desired_coordinates)
     end
 
 end
--- lib/game.rb
+++ lib/game.rb
@@ -11,17 +11,7 @@ class Game
 
     def legal_move?(current_coordinates, desired_coordinates)
         chess_piece = square(current_coordinates).piece
-        index = chess_piece.get_index_from_coordinates([current_coordinates])
-        chess_piece.adjacency_list[index].include?(desired_coordinates)
+        chess_piece.get_legal_moves(current_coordinates).include?(desired_coordinates)
     end
-
-    def can_make_move?(current_coordinates, desired_coordinates)
-        legal_move?(current_coordinates,desired_coordinates) && square(desired_coordinates).empty?
-    end
-
-    def square(coordinates_arr)
-        row = coordinates_arr[1]
-        column = coordinates_arr[0]
-        @game_board.object_board[7 - row][column]
-    end
+    
 end 
