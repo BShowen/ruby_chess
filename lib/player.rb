7f694d2ceb54b5f9b4c54f3c8fb194d9c9a3970a
Bradley Showen <BShowen@me.com>
2019-08-12 17:04:21 -0400

    add additional tests to player class. completed and passing

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,9 @@
 require_relative "custom_error"
+require_relative "custom_enumerable"
 
 class Player
 
+    include CustomEnumerable
     attr_accessor  :name, :color
 
     def initialize(name, color)
@@ -10,12 +12,14 @@ class Player
     end
 
     def solicit_move
-        # puts "Choose a piece to move by selecting the xy coordinate"
+        puts "Choose a piece to move by selecting the xy coordinate"
         begin
             reply = gets.chomp[0..1]
             ensure_reply_was_given(reply)
             x_coordinate = validate_x_coordinate(reply[0])
             y_coordinate = validate_y_coordinate(reply[1])
+            is_selected_square_blank?([x_coordinate,y_coordinate])
+            is_selected_square_opponents_piece?([x_coordinate,y_coordinate])
         rescue HumanInputError => e
             puts e.message
             retry 
@@ -27,15 +31,6 @@ class Player
     def ensure_reply_was_given(reply)
         raise HumanInputError, "You must type something" if /\w+/.match(reply) == nil
     end
-
-        def validate_y_coordinate(y_coord)
-            raise HumanInputError, "You didnt enter a Y coordinate" if y_coord.nil?
-            # raise HumanInputError, "Y coordinate must be a number" if /\D/.match(y_coord)
-            Integer(y_coord) rescue raise HumanInputError, "Y coordinate must be a number" 
-            y_coord = y_coord.to_i
-            raise HumanInputError, "Y coordinate is out of range" if y_coord < 0 || y_coord > 7
-            y_coord
-        end
 
     def validate_x_coordinate(x_coord)
         raise HumanInputError, "X coordinate must be a letter" if /\d/.match(x_coord)
@@ -59,5 +54,33 @@ class Player
         else
             raise HumanInputError, "X coordinate is out of range"
         end
+    end
+
+    def validate_y_coordinate(y_coord)
+        ensure_y_coord_is_given(y_coord)
+        y_coord = convert_to_integer(y_coord)
+        ensure_y_is_in_range(y_coord)
+        y_coord
+    end
+
+    def is_selected_square_blank?(coordinates)
+       raise HumanInputError, "That square is empty. Select a playing piece" if  square(coordinates).empty? 
+    end
+
+    def is_selected_square_opponents_piece?(coordinates)
+        raise HumanInputError, "You cannot select opponents pieces" if square(coordinates).piece.color != self.color
+    end
+
+    private
+    def ensure_y_coord_is_given(y_coord)
+        raise HumanInputError, "You didnt enter a Y coordinate" if y_coord.nil?
+    end
+
+    def convert_to_integer(y_coord)
+        Integer(y_coord) rescue raise HumanInputError, "Y coordinate must be a number" 
+    end
+
+    def ensure_y_is_in_range(y_coord)
+        raise HumanInputError, "Y coordinate is out of range" if y_coord < 0 || y_coord > 7
     end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,10 +1,13 @@
 require "./lib/player.rb"
+require "./lib/chess_board.rb"
 
 RSpec.describe "Player" do
     let(:player){Player.new("John",'white')}
+    let(:chess_board){ChessBoard.new}
 
     context "#solicit_move" do 
         it "solicits humnan for coordinates to move a chess piece" do 
+            chess_board
             allow(player).to receive(:gets){"d1"}
             expect(player.solicit_move).to eql([3,1])
         end
@@ -40,9 +43,19 @@ RSpec.describe "Player" do
         end
     end
 
-    context "#legal_selection?" do 
-        xit "doesnt allow selection of opponents chess pieces" do 
+    context "#is_selected_square_opponents_piece?" do 
+        it "doesnt allow selection of opponents chess pieces" do 
+            expect{ player.is_selected_square_opponents_piece?([3,7]) }.to raise_error(HumanInputError, "You cannot select opponents pieces")
+        end
+    end
 
+    context "#is_selected_square_blank?" do
+        it "raises error when human selects a blank square" do
+            expect{ player.is_selected_square_blank?([3,2]) }.to raise_error(HumanInputError, "That square is empty. Select a playing piece")
+        end
+
+        it "does not raise error when selected square is not blank" do 
+            expect{ player.is_selected_square_blank?([3,1]) }.to_not raise_error
         end
     end
 
