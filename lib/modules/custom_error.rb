84fd9a8d9b8983e7fd358f740c80950ae61e1612
Bradley Showen <BShowen@me.com>
2019-08-18 12:02:27 -0400

    add constructor to custom error

--- lib/modules/custom_error.rb
+++ lib/modules/custom_error.rb
@@ -1,3 +1,26 @@
 class HumanInputError < StandardError
-    
+    def initialize(msg)
+        case msg
+        when "opponents piece"
+            super("You cannot select opponents pieces")
+        when "blank"
+            super("That square is empty. Select a playing piece")
+        when"not possible"
+            super("Move is not possible")
+        when "illegal"
+            super("That is an illegal move")
+        when "blank reply"
+            super("You must type something")
+        when "invalid x coord"
+            super("X coordinate must be a letter")
+        when "x out of range"
+            super("X coordinate is out of range")
+        when "no y coord"
+            super("You didnt enter a Y coordinate")
+        when "invalid y coord"
+            super("Y coordinate must be a number")
+        when "y out of range"
+            super("Y coordinate is out of range")
+        end
+    end
 end
