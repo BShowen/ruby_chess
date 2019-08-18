eb4cb5bd7018a6f4568d8f2f8f3cb977b97d77b6
Bradley Showen <BShowen@me.com>
2019-08-18 11:34:39 -0400

    ChessPiece attr_accessor :current_position changed to method that toggles @moved = boolean when called

--- lib/chess_piece_classes/basic_chess_piece.rb
+++ lib/chess_piece_classes/basic_chess_piece.rb
@@ -1,6 +1,7 @@
 class BasicChessPiece 
     
-    attr_accessor :character, :color, :current_position, :moved
+    attr_accessor :character, :color, :moved
+    attr_reader :current_position
 
     def initialize(character, color, current_position)
         @character = character
@@ -8,13 +9,18 @@ class BasicChessPiece
         @current_position = current_position 
         @moved = false
     end
+
+    def current_position=(coords)
+        @current_position = coords  
+        @moved = true
+    end 
     
     # def constraints_are_met?(x,y)
     #     square([x, y]).empty? == true || square([x, y]).piece.color != self.color
     # end
 
-    def legal_move?(desired_coordinates)
-        self.get_legal_moves(self.current_position).include?(desired_coordinates)
-    end
+    # def legal_move?(desired_coordinates)
+    #     self.get_legal_moves(self.current_position).include?(desired_coordinates)
+    # end
 
 end
